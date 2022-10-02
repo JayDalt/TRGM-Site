c0b21c5ab93f468ed3a9a0c1037ab870d6213261
Jay Daltrey <jaydaltrey@gmail.com>
2022-10-02 17:08:01 +0100

    Add loadout logic

--- TRGM Site/Pages/CustomLoadOutPage.cshtml.cs
+++ TRGM Site/Pages/CustomLoadOutPage.cshtml.cs
@@ -27,17 +27,52 @@ public void OnPost()
     loadoutRequest.TextInput = TextInput;
     loadoutRequest.UnitType = UnitType;
 
-    ResultText = CustomLoadOutGenerator(loadoutRequest);
+    ResultText = CustomLoadoutGenerator(loadoutRequest);
 }
+        private string CustomLoadoutGenerator(LoadoutRequest request)
+        {
+            //add API code locally (temp)
 
-    public string CustomLoadOutGenerator(LoadoutRequest something)
-    {
-        return null;
+            string sResult = "";
+            string ResultText = "";
+            foreach (string strLine in request.TextInput.Split(Environment.NewLine))
+            {
+                int iInnerLoopCount = 0;
+                string adjustedString = strLine.Replace(Environment.NewLine, "").Replace("\n", "");
 
-        //add API code locally (temp)
-    }
+                if (adjustedString.Substring(0, 7) != "comment" && adjustedString != "" && adjustedString.Substring(0, 6) != "remove")
+                {
+                    string sTempResult = adjustedString;
+                    if (adjustedString.Substring(0, 3) == "for")
+                    {
+                        string sLoopCount = adjustedString.Substring(adjustedString.IndexOf("from 1 to ") + 10);
+                        sLoopCount = sLoopCount.Substring(sLoopCount.IndexOf(" do")).Trim();
+                        if (sLoopCount.All(char.IsNumber))
+                            iInnerLoopCount = Convert.ToInt32(sLoopCount) - 1;
+                        sTempResult = adjustedString.Substring(adjustedString.IndexOf("{") + 1, adjustedString.Length);
+                        sTempResult = sTempResult.Substring(0, sTempResult.IndexOf("}") - 1);
+                    }
+                    sTempResult = sTempResult.Replace("this ", "");
+                    sTempResult = sTempResult.Replace(" \"", "=");
+                    sTempResult = sTempResult.Replace("\";", ";");
 
-    public string LoadoutRequest()
+                    for (int i = 0; i <= iInnerLoopCount; i++)
+                        sResult += sTempResult + Environment.NewLine;
+                }
+            }
+            request.TextInput = "";
+            return ResultText = "#" + Environment.NewLine + request.UnitType + ":" + Environment.NewLine + sResult;
+
+        }
+
+
+        /* public string CustomLoadOutGenerator(LoadoutRequest something)
+         {
+             return null;
+
+         }*/
+
+        public string LoadoutRequest()
     {
         return null;
     }
